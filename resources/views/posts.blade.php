5c5b8efab45eb95fc7802f878fc54d15f3bab6bd
Bradley Evans <bradleyt.evans@gmail.com>
2023-01-03 14:29:04 -0500

    Stored blog posts as html files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,24 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Blog</title>
-    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="/app.css">
 </head>
 
 <body>
     <article>
-        <h1><a href="/post">First Post</a></h1>
+        <h1><a href="/posts/my-first-post">First Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsam praesentium quam numquam aliquid ut eos, expedita sequi vitae itaque. Adipisci rerum nisi nostrum, id impedit consequatur sunt! Quisquam, adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum labore error minima omnis doloremque pariatur aliquid mollitia, dolore vero neque perferendis ex! Delectus odio pariatur quae in perferendis excepturi consequatur.
         </p>
     </article>
     <article>
-        <h1><a href="/post">Second Post</a></h1>
+        <h1><a href="/posts/my-second-post">Second Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsam praesentium quam numquam aliquid ut eos, expedita sequi vitae itaque. Adipisci rerum nisi nostrum, id impedit consequatur sunt! Quisquam, adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum labore error minima omnis doloremque pariatur aliquid mollitia, dolore vero neque perferendis ex! Delectus odio pariatur quae in perferendis excepturi consequatur.
         </p>
     </article>
     <article>
-        <h1><a href="/post">Third Post</a></h1>
+        <h1><a href="/posts/my-third-post">Third Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsam praesentium quam numquam aliquid ut eos, expedita sequi vitae itaque. Adipisci rerum nisi nostrum, id impedit consequatur sunt! Quisquam, adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum labore error minima omnis doloremque pariatur aliquid mollitia, dolore vero neque perferendis ex! Delectus odio pariatur quae in perferendis excepturi consequatur.
         </p>
