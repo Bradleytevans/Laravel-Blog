e81614a5c90079096268a15d7d80ee466bfec8d2
Bradley Evans <bradleyt.evans@gmail.com>
2023-01-03 16:53:42 -0500

    routed wildcard constraints

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ Route::get('/', function () {
 
 Route::get('posts/{post}', function ($slug) {
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if (! file_exists($path)) {
+    if (!file_exists($path)) {
         return redirect('/');
     }
 
@@ -28,4 +27,4 @@ Route::get('posts/{post}', function ($slug) {
     return view('post', [
         'post' =>  "$post"
     ]);
-});
+})->where('post', '[A-z_\-]+');
