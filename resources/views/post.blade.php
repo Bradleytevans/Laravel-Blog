e2820509831e08dd2a8f2bfa300b852dcc2ef303
Bradley Evans <bradleyt.evans@gmail.com>
2023-01-03 13:04:57 -0500

    Made a route and linked to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsam praesentium quam numquam aliquid ut eos, expedita sequi vitae itaque. Adipisci rerum nisi nostrum, id impedit consequatur sunt! Quisquam, adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum labore error minima omnis doloremque pariatur aliquid mollitia, dolore vero neque perferendis ex! Delectus odio pariatur quae in perferendis excepturi consequatur.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
+
+</html>
