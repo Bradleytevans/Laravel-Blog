b2c64af5ac13e17f05fb1670f7774d0587b096b6
Bradley Evans <bradleyt.evans@gmail.com>
2023-01-10 18:20:42 -0500

    Added a User relationship to Post model; a Post relationship to User model; a user_id foreign key to create_posts migration. Created a Factories for Category and Post, and updated PostFactory to trigger Category and User factories. Updated DatabaseSeeder to reflect the ladder. Updated my views to display the author

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +18,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $user1 = User::factory()->create([
+            'name' => 'z0mbiebrad'
+        ]);
+        $cats1 = Category::factory()->create([
+            'slug' => 'odio'
+        ]);
+        Post::factory(10)->create([
+            'user_id' => $user1->id,
+            'category_id' => $cats1
+        ]);
+        $user2 = User::factory()->create([
+            'name' => 'fullstackalien'
+        ]);
+        $cats2 = Category::factory()->create([
+            'slug' => 'infernum'
+        ]);
+        Post::factory(10)->create([
+            'user_id' => $user2->id,
+            'category_id' => $cats2
+        ]);
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,8 +7,11 @@
     </article>
 
     <p>
-        <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        By <a href="">{{ $post->user->name }}</a> in <a
+            href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
     </p>
+
+
 
     <a href="/">Go Back</a>
 </x-layout>
