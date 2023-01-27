c4ace88f149b622cb2876cb4e9e6c7ed8ca4e29b
Bradley Evans <bradleyt.evans@gmail.com>
2023-01-27 10:54:55 -0500

    updated layout of the posts to have a featured post, 2 larger displays underneath, and the rest even smaller below that. Using the grid-span style attribute. Then moved that logic to its own component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,40 +1,14 @@
 <x-layout>
     @include('_posts-header')
 
-    {{-- Main Article --}}
     <main class="mx-auto mt-6 max-w-6xl space-y-6 lg:mt-20">
-        <x-post-featured-card></x-post-featured-card>
+        @if($posts->count())
 
-        {{-- Second Row --}}
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
+        <x-posts-grid :posts="$posts"></x-posts-drig>
 
-        {{-- Third Row --}}
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
-    </main>
-    {{-- @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {!! $post->title !!}
-                </a>
-            </h1>
-            <p>
-                By <a href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a
-                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
+        @else
+        <p class="text-center">No posts yet please check back.</p>
+        @endif
 
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-
-            <a href="/">Go Back</a>
-        </article>
-    @endforeach --}}
+    </main>`
 </x-layout>
