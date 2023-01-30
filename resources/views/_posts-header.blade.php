475d678db7a7115158a9f1411e58778285024234
Bradley Evans <bradleyt.evans@gmail.com>
2023-01-30 13:07:17 -0500

    extracted some logic from the categories drop down, made components for dropdown, dropdown-item, and icon. Gave categories and home a route name

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -14,37 +14,27 @@
     <div class="mt-8 space-y-2 lg:space-y-0 lg:space-x-4">
         <!--  Category -->
         <div class="relative rounded-xl bg-gray-100 lg:inline-flex">
-            <div x-data="{ show: false }" @click.away="show = false">
-                <button @click="show = ! show"
-                    class="inline-flex w-full flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-left text-sm font-semibold lg:w-32">
+            <x-dropdown>
+                <x-slot name="trigger">
+                    <button
+                        class="inline-flex w-full flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-left text-sm font-semibold lg:w-32">
 
-                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
+                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
-                    <svg class="pointer-events-none absolute -rotate-90 transform" style="right: 12px" width="22"
-                        height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                            </path>
-                            <path fill="#222"
-                                d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                        </g>
-                    </svg>
-                </button>
-
-                <div x-show="show" class="absolute z-50 mt-2 w-full rounded-xl bg-gray-100 py-2" style="display: none">
-                    <a href="/"
-                        class="block rounded-md px-3 text-left text-sm leading-6 hover:bg-blue-500 hover:text-white focus:bg-blue-500">
-                        All
-                    </a>
-                    @foreach ($categories as $category)
-                        <a href="/category/{{ $category->slug }}"
-                            class="{{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }} block rounded-md px-3 text-left text-sm leading-6 hover:bg-blue-500 hover:text-white focus:bg-blue-500 focus:text-white">
-                            {{ ucwords($category->name) }}
-                        </a>
-                    @endforeach
-                </div>
-
-            </div>
+                        <x-icon name="down-arrow" class="pointer-events-none absolute" style="right: 12px"></x-icon>
+                    </button>
+                </x-slot>
+                <x-dropdown-item href="/" :active="request()->routeIs('home')">
+                    All
+                </x-dropdown-item>
+                @foreach ($categories as $category)
+                    <x-dropdown-item
+                        href="/category/{{ $category->slug }}"
+                        :active="isset($currentCategory) && $currentCategory->is($category)"
+                        >{{ ucwords($category->name) }}
+                    </x-dropdown-item>
+                @endforeach
+            </x-dropdown>
         </div>
 
         <!-- Other Filters -->
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
@@ -36,7 +36,7 @@ Route::get('category/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('author/{author:username}', function (User $author) {
     return view('posts', [
