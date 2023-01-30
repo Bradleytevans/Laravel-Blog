364cfbb721dffdaceb02d8c01bbc8b01b9618ba0
z0mbiebrad <87036647+z0mbiebrad@users.noreply.github.com>
2023-01-30 17:08:59 -0500

    Created a Post Controller and enabled searching
    Moved logic from the routes file to the Post Controller, and refactored the routes file to call to the controller. Also created a query scope for searching and updated home route to be able to call on the filterScope.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
@@ -17,30 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('category/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
+Route::get('category/{category:slug}', [PostController::class, 'categories'])->name('catgegories');
 
-Route::get('author/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts,
-        'categories' => Category::all()
-    ]);
-});
+Route::get('author/{author:username}', [PostController::class, 'authors']);
