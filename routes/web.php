82141f330f795bfc411ef47c5675a428cf9fffd9
z0mbiebrad <87036647+z0mbiebrad@users.noreply.github.com>
2023-04-18 14:53:06 -0400

    Update web.php
    Got rid of old routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
-use App\Models\Post;
-use App\Models\Category;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
